2fcfc929fc346755ae4ad444740f6a8db0864672
AdamWestcott <72984379+AdamWestcott@users.noreply.github.com>
2021-12-16 14:39:58 +0000

    Added Hide and show for iframe functionality
    Added Hide and show for iframe functionality

--- RiskRegister/wwwroot/myJs.js
+++ RiskRegister/wwwroot/myJs.js
@@ -1,53 +1,5 @@
-﻿////function saveAsFile(fileName, bytesBase64) {
-
-////    var link = document.createElement('a');
-
-////    link.download = fileName;
-
-////    link.href = 'data:application/pdf;base64,' + bytesBase64;
-
-////    document.body.appendChild(link);
-
-////    link.click();
-
-////    document.body.removeChild(link);
-////}
-
-//function ShowPDF(fileName, filePromise) {
-//    document.addEventListener("adobe_dc_view_sdk.ready")
-//    type = "text/javascript";
-//    var adobeDCView = new AdobeDC.View({ clientId: "d177e1afd072400a94115f298f0e92a2", divId: "adobe-dc-view" });
-//    adobeDCView.previewFile({
-//        content: {location: { url: "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf" } },
-//        metaData: { fileName: fileName }
-//    }, viewerOptions, { embedMode: "IN_LINE", showDownloadPDF: false, showPrintPDF: false });
-//}
-
-const clientId = "d177e1afd072400a94115f298f0e92a2";
-
-//var filePromise, buttonsDiv = document.getElementById("showPdf");
-
-
-//document.addEventListener("adobe_dc_view_sdk.ready", function () {
-//        var adobeDCView = new AdobeDC.View({ clientId: "d177e1afd072400a94115f298f0e92a2", divId: "adobe-dc-view" });
-//        adobeDCView.previewFile({
-//            content: { promise: Promise.resolve(base64ToArrayBuffer(filePromise)) },
-//            metaData: { fileName: "Bodea Brochure.pdf" }
-//        }, { embedMode: "IN_LINE", showDownloadPDF: false, showPrintPDF: false });
-//    });
-//document.getElementById("showPDF").addEventListener("click", function () {
-//    showPDF(filePromise)
-//});
+﻿
 function base64ToArrayBuffer(base64) {
-    //var bin = window.atob(base64);
-    //var len = bin.length;
-    //var uInt8Array = new Uint8Array(len);
-    //for (var i = 0; i < len; i++) {
-    //    uInt8Array[i] = bin.charCodeAt(i);
-    //}
-
-    //document.getElementById("myFrame").src = uInt8Array.buffer;
-
     var byteCharacters = atob(base64);
     var byteNumbers = new Array(byteCharacters.length);
     for (var i = 0; i < byteCharacters.length; i++) {
@@ -58,32 +10,27 @@ function base64ToArrayBuffer(base64) {
 
     //Convert blob into URL Object
     blob = window.URL.createObjectURL(blob);
-    document.getElementById("myFrame").src = blob;
-   // iframe = container.find('iframe')[0];
-    //return uInt8Array.buffer;
+    var x = document.getElementById("myFrame");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    document.getElementById("myFrame").src = blob + "#toolbar=0";
 }
 
-function reloadJs(src) {
-    src = $('script[src$="' + src + '"]').attr("src");
-    $('script[src$="' + src + '"]').remove();
-    $('<script/>').attr('src', src).appendTo('head');
+function HideShowIframe() {
+    var x = document.getElementById("myFrame");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
 
-function showPDF(filename, filePromise) {
-    var adobeDCView = new AdobeDC.View({
-        clientId: clientId, divId: "adobe-dc-view"
-    });
-    var previewFilePromise = adobeDCView.previewFile(
-        {
-            content: { promise: Promise.resolve(base64ToArrayBuffer(filePromise)) },
-            metaData: { fileName: "Bodea Summary.pdf" }
-        },
-        { embedMode: "IN_LINE", showDownloadPDF: false, showPrintPDF: false }
-    );
+function HideIframeOnDelete() {
+    var x = document.getElementById("myFrame");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    }
 }
 
-//document.addEventListener("adobe_dc_view_sdk.ready", function () {
-//    document.getElementById("showPDF").addEventListener("click", function () {
-//        showPDF("https://documentcloud.adobe.com/view-sdk-demo/PDFs/Summary.pdf")
-//    });
-//});
+
